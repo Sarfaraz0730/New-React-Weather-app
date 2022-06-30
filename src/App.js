29a7fab6a86f73c2a7d432dc3de288613fdffff2
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-07-01 00:09:55 +0530

    added a header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>Hy I am very Happy</h3>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
