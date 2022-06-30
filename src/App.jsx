9cad6dbad2c11aed8753a9c16342bd34b8242839
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-07-01 00:45:21 +0530

    Geo location of user fetched

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchLocation from './Components/SearchLocation';
+import GeoLocationOfUser from './Components/GeoLocationOfUser';
+
+function App() {
+  const location =GeoLocationOfUser()
+  return (
+    <div className="App">
+      <SearchLocation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SearchLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchLocation = () => {
+  return (
+    <div>
+      <h1>Location</h1>
+    </div>
+  )
+}
+
+export default SearchLocation
