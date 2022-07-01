de07f8c8fe16fce891550c22101a6310e2d7302e
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-07-01 22:57:30 +0530

    weekly data is coming

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import SearchLocation from './Components/SearchLocation';
 import GeoLocationOfUser from './Components/GeoLocationOfUser';
 
+
 function App() {
   const location =GeoLocationOfUser()
   return (
     <div className="App">
-      <SearchLocation/>
+      <SearchLocation />
+   
     </div>
   );
 }
--- src/Components/SearchLocation.jsx
+++ src/Components/SearchLocation.jsx
@@ -1,13 +1,43 @@
 import "../Style/SearchLocation.css";
 import Search_Icon_svg from "../Assests/Search_Icon.svg.png";
+
 import location from "../Assests/location.jpg"
+import { useState } from "react";
+import { useEffect } from "react";
+import WeeklyForecast from "./WeeklyForecast";
+
 const SearchLocation = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Delhi");
+  const [icons, setIcons] = useState("");
+  const [wind, setWind] = useState("");
+
+
+  useEffect(() => {
+     const fetchApi = async () => {
+       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&&units=metric&appid=35dda4b74680a4f66fe72fd65ba4569d`;
+
+       const response = await fetch(url);
+
+       const resJson = await response.json();
+       console.log("resjson", resJson);
+
+       setCity(resJson.main);
+       setIcons(resJson.weather[0]);
+       setWind(resJson.wind);
+
+       console.log("Current temp",resJson.main.temp)
+     };
+
+     fetchApi();
+   },[search])
+  
   return (
     <>
       <section>
         <div className="container">
           <div className="searchDiv">
-           <img className="locationIcon"  src= {location} alt="" />
+            <img className="locationIcon" src={location} alt="" />
             <input
               id="searchLocation"
               type="text"
@@ -16,6 +46,18 @@ const SearchLocation = () => {
             />
             <img className="searchIcon" src={Search_Icon_svg} alt="" />
           </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="Daily-data">
+          <WeeklyForecast search={ search} />
+        </div>
+      </section>
+
+      <section>
+        <div className="Daily-data">
+  
         </div>
       </section>
     </>
